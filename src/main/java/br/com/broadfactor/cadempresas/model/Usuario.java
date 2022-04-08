649aba27353e5dc351cd805254ab8222e3104e33
Willams S. de Sousa <osdeving@gmail.com>
2022-04-07 21:00:07 -0300

    relacionou usuario com empresa

--- src/main/java/br/com/broadfactor/cadempresas/model/Usuario.java
+++ src/main/java/br/com/broadfactor/cadempresas/model/Usuario.java
@@ -1,28 +1,33 @@
 package br.com.broadfactor.cadempresas.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
+@Getter
+@Setter
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private Integer id;
 
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "email", unique = true)
     private String email;
+
+    @Column(name = "cnpj")
     private String cnpj;
+
+    @Column(name = "senha")
     private String senha;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToOne(cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+    private Empresa empresa;
 }
