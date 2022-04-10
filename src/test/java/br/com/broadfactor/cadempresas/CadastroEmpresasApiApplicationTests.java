1f49bd6620d88207922c55cc777f3c198c492f93
Willams S. de Sousa <osdeving@gmail.com>
2022-04-09 22:34:57 -0300

    inseriu usuário e senha no banco para testes

--- src/test/java/br/com/broadfactor/cadempresas/CadastroEmpresasApiApplicationTests.java
+++ src/test/java/br/com/broadfactor/cadempresas/CadastroEmpresasApiApplicationTests.java
@@ -2,12 +2,17 @@ package br.com.broadfactor.cadempresas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class CadastroEmpresasApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String encodeded = encoder.encode("123456");
+		System.out.println(encodeded);
 	}
 
 }
